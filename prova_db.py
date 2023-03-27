cae4ff6cd7ca3e43e30ef0c48a31bddc0b1ee00d
marzowill96 <129086681+marzowill96@users.noreply.github.com>
2023-03-27 18:48:30 +0200

    Add files via upload

---
+++ prova_db.py
@@ -0,0 +1,26 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+app = dash.Dash(__name__)
+
+app.layout = html.Div(children=[
+    html.H1('My Dashboard'),
+    dcc.Graph(
+        id='example-graph',
+        figure={
+            'data': [
+                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'Bar chart'},
+                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'line', 'name': 'Line chart'},
+            ],
+            'layout': {
+                'title': 'Graphs'
+            }
+        }
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server()
+    
+    
