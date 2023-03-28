dd38e7715ec63b9f20cd8993bc2deb1545bf9a06
marzowill96 <129086681+marzowill96@users.noreply.github.com>
2023-03-28 13:58:56 +0200

    Update git_db_iis.py

--- git_db_iis.py
+++ git_db_iis.py
@@ -42,38 +42,36 @@ for t in base_dati_weekly.index:
 
 base_dati_weekly = base_dati_weekly.apply(pd.to_numeric) 
 
-
-
-# fondi = pd.read_excel(url)
-# names = fondi.columns[1:]
+ names = fondi.columns[1:]
  
-#  # associo ISIN a Nome Fondo per tutti i fondi
-# names_dict = dict(zip(fondi[fondi.columns[1:]].iloc[1], fondi.columns[1:]))
-# for key, value in names_dict.items():
-#     names_dict[key] = value.replace('Mediolanum Best Brands', 'MBB') 
+  # associo ISIN a Nome Fondo per tutti i fondi
+names_dict = dict(zip(fondi[fondi.columns[1:]].iloc[1], fondi.columns[1:]))
+for key, value in names_dict.items():
+    names_dict[key] = value.replace('Mediolanum Best Brands', 'MBB') 
     
-# isin_dict_0 = dict(zip(fondi.columns[1:], fondi[fondi.columns[1:]].iloc[1]))
+isin_dict_0 = dict(zip(fondi.columns[1:], fondi[fondi.columns[1:]].iloc[1]))
 
-# isin_dict = {}
-# for key, value in isin_dict_0.items():
-#     new_key = key.replace('Mediolanum Best Brands', 'MBB')
-#     new_value = value.replace('Mediolanum Best Brands', 'MBB')
-#     isin_dict[new_key] = new_value
+isin_dict = {}
+for key, value in isin_dict_0.items():
+    new_key = key.replace('Mediolanum Best Brands', 'MBB')
+    new_value = value.replace('Mediolanum Best Brands', 'MBB')
+    isin_dict[new_key] = new_value
 
-# # Define list of dates for dropdown menu
+# Define list of dates for dropdown menu
 
-# dates = list(base_dati_weekly.index)
-# for t in range(len(dates)):
-#     dates[t] = dt.strftime(dates[t], "%d/%m/%Y")
+dates = list(base_dati_weekly.index)
+for t in range(len(dates)):
+    dates[t] = dt.strftime(dates[t], "%d/%m/%Y")
     
-# fondi_necessari = ['IE00BYZ2Y955','IE00BYZ2YB75','IE0005380518','IE0032080503','IE00B04KP775','IE00B2NLMT64','IE00B2NLMV86','IE00BCZNHK63','IE0004621052','IE0032082988','IE0030608859']
-# nomi = [names_dict[key] for key in fondi_necessari]
+fondi_necessari = ['IE00BYZ2Y955','IE00BYZ2YB75','IE0005380518','IE0032080503','IE00B04KP775','IE00B2NLMT64','IE00B2NLMV86','IE00BCZNHK63','IE0004621052','IE0032082988','IE0030608859']
+nomi = [names_dict[key] for key in fondi_necessari]
 
-# performance_df = pd.DataFrame(index=[0], columns = ['Performance','IIS','PIC','Effetto Strategia','Prezzo Iniziale','Prezzo Finale','Prezzo Medio','Rimbalzo per parità IIS','Rimbalzo per parità PIC'])
-# vol_df = pd.DataFrame(index=[0], columns = ['Volatilità','IIS','PIC','Effetto Strategia'])
-# maxdd_df = pd.DataFrame(index=[0], columns = ['Max Draw-Down','IIS','PIC','Effetto Strategia'])
-# step_in_df = pd.DataFrame(index=[0,1,2,3,4], columns=['Step - In', 'Conteggio'])
-# step_out_df = pd.DataFrame(index=[0], columns=['Step - Out', 'Conteggio']) 
+performance_df = pd.DataFrame(index=[0], columns = ['Performance','IIS','PIC','Effetto Strategia','Prezzo Iniziale','Prezzo Finale','Prezzo Medio','Rimbalzo per parità IIS','Rimbalzo per parità PIC'])
+vol_df = pd.DataFrame(index=[0], columns = ['Volatilità','IIS','PIC','Effetto Strategia'])
+maxdd_df = pd.DataFrame(index=[0], columns = ['Max Draw-Down','IIS','PIC','Effetto Strategia'])
+step_in_df = pd.DataFrame(index=[0,1,2,3,4], columns=['Step - In', 'Conteggio'])
+step_out_df = pd.DataFrame(index=[0], columns=['Step - Out', 'Conteggio']) 
+
 
 
 app = dash.Dash(__name__)
